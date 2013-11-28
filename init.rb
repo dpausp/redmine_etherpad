5de128f5704d84901d934a71de36de10a3339888
Tobias Jungel <tobias.jungel@eict.de>
2013-11-28 15:39:01 +0100

    etherpad session re-use and SSL certificate verification
    * re-use session if its still valid for etherpad (session checked via cookie and etherpad api)
    * verifySSL: true/false added to access pages with self-signed certificates
    * move method out of block scope for ruby 1.9
    * documentation updated
    * .gitignore updated to ignore dotfiles besides .gitignore
    * tested with ruby 1.8.7, redmine 2.3.3, etherpad 1.3.0

--- init.rb
+++ init.rb
@@ -9,20 +9,27 @@ require 'json'
 require 'time'
 require 'date'
 
+def hash_to_querystring(hash)
+  hash.keys.inject('') do |query_string, key|
+    query_string << '&' unless key == hash.keys.first
+    query_string << "#{URI.encode(key.to_s)}=#{URI.encode(hash[key].to_s)}"
+  end
+end
+
 Redmine::Plugin.register :redmine_etherpad_auth do
   name 'Redmine Etherpad plugin with Group Authentication'
-  author 'Martin Draexler'
-  description 'Embed etherpad-lite pads in redmine wikis with authentication. Based on the redmine_etherpad plugin by Charlie DeTar'
-  version '0.0.1'
-  url 'https://upb.de'
-  author_url 'https://upb.de'
+  author 'Tobias Jungel'
+  description 'Embed etherpad-lite pads in redmine wikis with authentication. Based on the redmine_etherpad plugin by Charlie DeTar and redmine_etherpad_auth by Martin Draexler'
+  version '0.1'
+  url 'https://github.com/toanju/redmine_etherpad'
+  author_url 'https://github.com/toanju'
 
   Redmine::WikiFormatting::Macros.register do
     desc "Embed etherpad with auth"
     macro :etherpadauth do |obj, args|
       conf = Redmine::Configuration['etherpadauth']
-      unless conf and conf['host'] 
-        raise "Please define etherpad parameters in configuration.yml."
+      unless conf and conf['host']
+        raise "Please define etherpadauth parameters in configuration.yml."
       end
 
       # Defaults from configuration.
@@ -34,7 +41,8 @@ Redmine::Plugin.register :redmine_etherpad_auth do
         'noColors' => conf.fetch('noColors', false),
         'width' => conf.fetch('width', '640px'),
         'height' => conf.fetch('height', '480px'),
-	    'apiKey' => conf.fetch('apiKey', 'xxx'),
+        'apiKey' => conf.fetch('apiKey', 'xxx'),
+        'verifySSL' => conf.fetch('verifySSL', true),
       }
 
       # Override defaults with given arguments.
@@ -63,44 +71,64 @@ Redmine::Plugin.register :redmine_etherpad_auth do
         return "Invalid obj context"
       end
 
+      uri = URI.parse(conf['host'])
+      http = Net::HTTP.new(uri.host, uri.port)
+      if uri.scheme.casecmp("https")
+        http.use_ssl = true
+      end
+      if not controls['verifySSL']
+        http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+      end
 
-      uri = URI(conf['host'] + '/api/1/createAuthorIfNotExistsFor')
-      params = { 'apikey' => controls['apiKey'], 'name' => controls['userName'], 'authorMapper' => controls['userId'] }
-      res = Net::HTTP.post_form(uri, params)
+      params = { 'apikey' => controls['apiKey'], 'name' => controls['userName'], 'authorMapper' => controls['userId'].to_s() }
+      res = http.post('/api/1/createAuthorIfNotExistsFor', hash_to_querystring(params))
       resdata = JSON.parse(res.body)
       controls['authorId'] = resdata['data']['authorID']
 
-      uri = URI(conf['host'] + '/api/1/createGroupIfNotExistsFor')
-      params = { 'apikey' => controls['apiKey'], 'groupMapper' => controls['projectId'] }
-      res = Net::HTTP.post_form(uri, params)
+      params = { 'apikey' => controls['apiKey'], 'groupMapper' => controls['projectId'].to_s() }
+      res = http.post('/api/1/createGroupIfNotExistsFor', hash_to_querystring(params))
       resdata = JSON.parse(res.body)
       controls['groupId'] = resdata['data']['groupID']
 
-      uri = URI(conf['host'] + '/api/1/createGroupPad')
-      params = { 'apikey' => controls['apiKey'], 'groupID' => controls['groupId'], 'padName' => padname }
-      res = Net::HTTP.post_form(uri, params)
+      params = { 'apikey' => controls['apiKey'], 'groupID' => controls['groupId'].to_s(), 'padName' => padname }
+      res = http.post('/api/1/createGroupPad', hash_to_querystring(params))
       resdata = JSON.parse(res.body)
-      controls['groupPad'] = resdata['data']['padID']
 
-      uri = URI(conf['host'] + '/api/1/createSession')
-      params = { 'apikey' => controls['apiKey'], 'groupID' => controls['groupId'], 'authorID' => controls['authorId'], 'validUntil' => Time.now.to_i+(60*60) }
-      res = Net::HTTP.post_form(uri, params)
-      resdata = JSON.parse(res.body)
-      controls['sessionId'] = resdata['data']['sessionID']
+      if resdata['code'] == 1
+        controls['groupPad'] = "#{controls['groupId']}$#{padname}"
+      else
+        controls['groupPad'] = resdata['data']['padID']
+      end
 
-      cookies[:sessionID] = { :value => controls['sessionId'], :domain => ".fg-cn-pgsp1.cs.upb.de"}
+      # session still valid?
+      update_session = false
+      if not cookies[:sessionID].nil?
+        params = { 'apikey' => controls['apiKey'], 'sessionID' => cookies[:sessionID] }
+        res = http.post('/api/1/getSessionInfo', hash_to_querystring(params))
+        resdata = JSON.parse(res.body)
+        if resdata['code'] == 1 or resdata['data']['validUntil'].to_i <= Time.now.to_i
+          update_session = true
+        end
+      end
+
+      # update/create session
+      if cookies[:sessionID].nil? or update_session
+        expires = Time.now + 3600
+        params = { 'apikey' => controls['apiKey'], 'groupID' => controls['groupId'], 'authorID' => controls['authorId'], 'validUntil' => expires.to_i }
+        res = http.post('/api/1/createSession', hash_to_querystring(params))
+        resdata = JSON.parse(res.body)
+        controls['sessionId'] = resdata['data']['sessionID']
+
+        cookies[:sessionID] = { :value => controls['sessionId'], :host => uri.host, :expires => expires }
+        if http.use_ssl
+          cookies[:sessionID] =  { :value => controls['sessionId'], :host => uri.host, :expires => expires, :secure => true }
+        end
+      end
 
       width = controls.delete('width')
       height = controls.delete('height')
 
-      def hash_to_querystring(hash)
-        hash.keys.inject('') do |query_string, key|
-          query_string << '&' unless key == hash.keys.first
-          query_string << "#{URI.encode(key.to_s)}=#{URI.encode(hash[key].to_s)}"
-        end
-      end
-      
-      return CGI::unescapeHTML("<iframe src='#{conf['host']}/p/#{URI.encode(controls['groupPad'])}?#{hash_to_querystring(controls)}' width='#{width}' height='#{height}'></iframe>")
+      return CGI::unescapeHTML("<iframe src='#{conf['host']}/p/#{URI.encode(controls['groupPad'])}?#{hash_to_querystring(controls)}' width='#{width}' height='#{height}'></iframe>").html_safe
     end
   end
 end
