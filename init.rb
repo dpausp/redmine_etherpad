bb79426869b99410766fa2823ec115552ae1fe6b
dpausp <dpausp@users.noreply.github.com>
2017-01-26 14:24:38 +0000

    large refactoring, preview placeholder, larger pad iframe
    -   factor out api calls and option processing to functions
    -   don't put everything into the options dict
        -   disallow overriding system settings like the api_key
    -   show placeholder instead of error message when using the preview function for new issues
    -   pad iframe uses 100% width and 600px height now by default

--- init.rb
+++ init.rb
@@ -6,12 +6,45 @@ require 'time'
 require 'date'
 require 'pry'
 
+
 def hash_to_querystring(hash)
   hash.keys.inject('') do |query_string, key|
     query_string << '&' unless key == hash.keys.first
     query_string << "#{URI.encode(key.to_s)}=#{URI.encode(hash[key].to_s)}"
   end
 end
+
+
+def etherpad_request(http, uri, endpoint, params)
+  res = http.post((uri.path.nil? ? '' : uri.path) + endpoint, hash_to_querystring(params))
+  JSON.parse(res.body)
+end
+
+
+def build_options(conf, params)
+    # Defaults from configuration.
+  # TODO: everything except height and width is ignored for now
+  options = {
+    'showControls' => conf.fetch('showControls', true),
+    'showChat' => conf.fetch('showChat', true),
+    'showLineNumbers' => conf.fetch('showLineNumbers', false),
+    'useMonospaceFont' => conf.fetch('useMonospaceFont', false),
+    'noColors' => conf.fetch('noColors', false),
+    'width' => conf.fetch('width', '100%'),
+    'height' => conf.fetch('height', '600px'),
+  }
+  # Override defaults with given arguments.
+  for param in params
+    key, val = param.strip().split("=")
+    unless controls.has_key?(key)
+      raise "#{key} not a recognized parameter."
+    else
+      defaults[key] = val
+    end
+  end
+  options
+end
+
 
 Redmine::Plugin.register :redmine_etherpad do
   name 'Redmine Etherpad (lite) plugin'
@@ -29,83 +62,64 @@ Redmine::Plugin.register :redmine_etherpad do
         raise "Please define etherpad parameters in configuration.yml."
       end
 
-      # Defaults from configuration.
-      controls = {
-        'showControls' => conf.fetch('showControls', true),
-        'showChat' => conf.fetch('showChat', true),
-        'showLineNumbers' => conf.fetch('showLineNumbers', false),
-        'useMonospaceFont' => conf.fetch('useMonospaceFont', false),
-        'noColors' => conf.fetch('noColors', false),
-        'width' => conf.fetch('width', '640px'),
-        'height' => conf.fetch('height', '480px'),
-        'apiKey' => conf.fetch('apiKey', 'xxx'),
-        'verifySSL' => conf.fetch('verifySSL', true),
-      }
+      unless obj
+        return "{{etherpad()}}"
+      end
 
-      # Override defaults with given arguments.
       padname, *params = args
-      for param in params
-        key, val = param.strip().split("=")
-        unless controls.has_key?(key)
-          raise "#{key} not a recognized parameter."
-        else
-          controls[key] = val
-        end
-      end
-
-      # Set current user name.
-      if User.current
-        controls['userName'] = User.current.name
-        controls['userId'] = User.current.id
-      elsif conf.fetch('loginRequired', true)
-        return "TODO: embed read-only."
-      end
-
-      if obj
-        controls['projectName'] = obj.project.name
-        controls['projectId'] = obj.project.identifier
-      else
-        return "Invalid obj context"
-      end
+      options = build_options(conf, params)
+      api_key = conf.fetch('apiKey', 'xxx')
+      verify_ssl = conf.fetch('verifySSL', true)
 
       uri = URI.parse(conf['host'])
-      #binding.pry
       http = Net::HTTP.new(uri.host, uri.port)
       unless uri.scheme.casecmp("https")
         http.use_ssl = true
       else
         http.use_ssl = false
       end
-      if not controls['verifySSL']
+      unless verify_ssl
         http.verify_mode = OpenSSL::SSL::VERIFY_NONE
       end
 
-      params = { 'apikey' => controls['apiKey'], 'name' => controls['userName'], 'authorMapper' => controls['userId'].to_s() }
-      res = http.post((uri.path.nil? ? '' : uri.path) + '/api/1/createAuthorIfNotExistsFor', hash_to_querystring(params))
-      resdata = JSON.parse(res.body)
-      controls['authorId'] = resdata['data']['authorID']
 
-      params = { 'apikey' => controls['apiKey'], 'groupMapper' => controls['projectId'].to_s() }
-      res = http.post((uri.path.nil? ? '' : uri.path) + '/api/1/createGroupIfNotExistsFor', hash_to_querystring(params))
-      resdata = JSON.parse(res.body)
-      controls['groupId'] = resdata['data']['groupID']
+      params = { 
+        'apikey' => api_key,
+        'name' => User.current.name,
+        'authorMapper' => User.current.id
+      }
 
-      params = { 'apikey' => controls['apiKey'], 'groupID' => controls['groupId'].to_s(), 'padName' => padname }
-      res = http.post((uri.path.nil? ? '' : uri.path) + '/api/1/createGroupPad', hash_to_querystring(params))
-      resdata = JSON.parse(res.body)
+      resdata = etherpad_request(http, uri, '/api/1/createAuthorIfNotExistsFor', params)
+      author_id = resdata['data']['authorID']
+
+      params = { 
+        'apikey' => api_key, 
+        'groupMapper' => obj.project.identifier
+      }
+      resdata = etherpad_request(http, uri, '/api/1/createGroupIfNotExistsFor', params)
+      group_id = resdata['data']['groupID']
+
+      params = { 
+        'apikey' => api_key, 
+        'groupID' => group_id,
+        'padName' => padname 
+      }
+      resdata = etherpad_request(http, uri, '/api/1/createGroupPad', params)
 
       if resdata['code'] == 1
-        controls['groupPad'] = "#{controls['groupId']}$#{padname}"
+        group_pad = "#{group_id}$#{padname}"
       else
-        controls['groupPad'] = resdata['data']['padID']
+        group_pad = resdata['data']['padID']
       end
 
       # session still valid?
       update_session = false
       if not cookies[:sessionID].nil?
-        params = { 'apikey' => controls['apiKey'], 'sessionID' => cookies[:sessionID] }
-        res = http.post((uri.path.nil? ? '' : uri.path) + '/api/1/getSessionInfo', hash_to_querystring(params))
-        resdata = JSON.parse(res.body)
+        params = { 
+          'apikey' => api_key, 
+          'sessionID' => cookies[:sessionID] 
+        }
+        resdata = etherpad_request(http, uri, '/api/1/getSessionInfo', params)
         if resdata['code'] == 1 or resdata['data']['validUntil'].to_i <= Time.now.to_i
           update_session = true
         else
@@ -116,22 +130,25 @@ Redmine::Plugin.register :redmine_etherpad do
       # update/create session
       if cookies[:sessionID].nil? or update_session
         expires = Time.now + 3600
-        params = { 'apikey' => controls['apiKey'], 'groupID' => controls['groupId'], 'authorID' => controls['authorId'], 'validUntil' => expires.to_i }
-        res = http.post((uri.path.nil? ? '' : uri.path) + '/api/1/createSession', hash_to_querystring(params))
-        resdata = JSON.parse(res.body)
-        controls['sessionId'] = session_id = resdata['data']['sessionID']
+        params = { 
+          'apikey' => api_key, 
+          'groupID' => group_id, 
+          'authorID' => author_id, 
+          'validUntil' => expires.to_i 
+        }
+        resdata = etherpad_request(http, uri, '/api/1/createSession', params)
+        session_id = resdata['data']['sessionID']
 
-        cookies[:sessionID] = { :value => controls['sessionId'], :host => uri.host, :expires => expires }
+        cookies[:sessionID] = { :value => session_id, :host => uri.host, :expires => expires }
         if http.use_ssl?
-          cookies[:sessionID] =  { :value => controls['sessionId'], :host => uri.host, :expires => expires, :secure => true }
+          cookies[:sessionID] =  { :value => session_id, :host => uri.host, :expires => expires, :secure => true }
         end
       end
 
-      width = controls.delete('width')
-      height = controls.delete('height')
-      controls.delete('apikey')
+      width = options.delete('width')
+      height = options.delete('height')
       
-      pad_url = "#{conf['host']}/auth_session?padName=#{URI.encode(controls['groupPad'])}&sessionID=#{session_id}"
+      pad_url = "#{conf['host']}/auth_session?padName=#{URI.encode(group_pad)}&sessionID=#{session_id}"
       return CGI::unescapeHTML("<a href='#{pad_url}' target='blank'>Pad in eigenem Fenster öffnen</a><br><iframe src='#{pad_url}' width='#{width}' height='#{height}'></iframe>").html_safe
     end
   end
